3a6298a7de8ce038b0c80c038d3fbada4f8fa643
Ashutosh Mishra <ashu23092002@gmail.com>
2025-03-06 13:08:55 +0530

    Camera Switch And Snapshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'dart:async';
-import 'dart:io' show Platform;
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:flutter/services.dart';
-
-late List<CameraDescription> _cameras;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,13 +11,14 @@ Future<void> main() async {
   if (kIsWeb) {
     runApp(MainApp(displayText: 'Web World'));
   } else if(Platform.isAndroid){
-    _cameras = await availableCameras();
-    final rearCamera = _cameras.first;
+    final cameras = await availableCameras();
     runApp(
-      CameraApp(
-        camera: rearCamera,
+      MaterialApp(
+        home:CameraApp(
+          cameras: cameras,
+        ),
       )
-      );
+    );
   } else {
     runApp(MainApp(displayText: 'Desktop World'));
   }
@@ -28,78 +26,193 @@ Future<void> main() async {
 
 /// Android Camera App
 class CameraApp extends StatefulWidget {
-  /// Default Constructor
-  const CameraApp({super.key, required this.camera});
+  final List<CameraDescription> cameras;
 
-  final CameraDescription camera;
+  /// Default Constructor
+  const CameraApp({super.key, required this.cameras});
+
 
   @override
-  State<CameraApp> createState() => _CameraAppState();
+  CameraScreenState createState() => CameraScreenState();
 }
 
-/// Camera Controller Initialization
-class _CameraAppState extends State<CameraApp> {
-  late CameraController? controller;
-  
+class CameraScreenState extends State<CameraApp> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  int _currentCameraIndex = 0;
+
   @override
   void initState() {
     super.initState();
-    controller = CameraController(_cameras[0], ResolutionPreset.max);
-    controller!.lockCaptureOrientation(DeviceOrientation.portraitUp);
-    controller!.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    }).catchError((Object e) {
-      if (e is CameraException) {
-        switch (e.code) {
-          case 'CameraAccessDenied':
-            // Handle access errors here.
-            break;
-          default:
-            // Handle other errors here.
-            break;
-        }
-      }
+    _initializeCamera(_currentCameraIndex);
+  }
+
+  /// Initialize camera based on index
+  void _initializeCamera(int cameraIndex) {
+    _controller = CameraController(
+      widget.cameras[cameraIndex],
+      ResolutionPreset.max,
+    );
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  /// Switch camera
+  void _switchCamera() {
+    setState(() {
+      _currentCameraIndex = (_currentCameraIndex + 1) % widget.cameras.length;
+      _initializeCamera(_currentCameraIndex);
     });
   }
 
   @override
   void dispose() {
-    controller!.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (controller == null || !controller!.value.isInitialized) {
-      return Container();
-    }
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Camera Application'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Center(
-                child: _cameraPreviewWidget(),
-              ),
-            ),
-          ],
-        ),
-      )
-    );
-  }
-
-  Widget _cameraPreviewWidget() {
-    return CameraPreview(
-      controller!,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Camera App'),),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture, 
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child:CircularProgressIndicator());
+          }
+        }
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        spacing: 10,
+        children: [
+          FloatingActionButton(
+            heroTag: 'take_picture',
+            onPressed: () async {
+              try {
+                await _initializeControllerFuture;
+                final image = await _controller.takePicture();
+                if(!mounted) {
+                  return;
+                }
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context) => DisplayPictureScreen(imagePath: image.path),
+                ));
+              } catch(e) {
+                print(e);
+              }
+            },
+            child: Builder(
+              builder: (context) {
+                double iconSize = MediaQuery.of(context).size.width * 0.1;
+                return Icon(
+                  Icons.camera,
+                  size: iconSize,
+                );
+              }
+            )
+          ),
+          FloatingActionButton(
+            onPressed: _switchCamera,
+            heroTag: 'switch_camera',
+            child: Builder(
+              builder: (context) {
+                double iconSize = MediaQuery.of(context).size.width * 0.1;
+                return Icon(
+                  Icons.switch_camera_outlined,
+                  size: iconSize,
+                );
+              }
+            )
+          ),
+        ]
+      ),
     );
   }
 }
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Last Picture'),),
+      body: Center(child: Image.file(File(imagePath)),),
+    );
+  }
+}
+
+/// Camera Controller Initialization
+// class _CameraAppState(CameraDescription camera) extends State<CameraApp> {
+//   late CameraController? controller;
+  
+//   _CameraAppState({super.key, required this});
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     controller = CameraController(camera, ResolutionPreset.max);
+//     controller!.lockCaptureOrientation(DeviceOrientation.portraitUp);
+//     controller!.initialize().then((_) {
+//       if (!mounted) {
+//         return;
+//       }
+//       setState(() {});
+//     }).catchError((Object e) {
+//       if (e is CameraException) {
+//         switch (e.code) {
+//           case 'CameraAccessDenied':
+//             // Handle access errors here.
+//             break;
+//           default:
+//             // Handle other errors here.
+//             break;
+//         }
+//       }
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     controller!.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     if (controller == null || !controller!.value.isInitialized) {
+//       return Container();
+//     }
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Camera Application'),
+//         ),
+//         body: Column(
+//           children: <Widget>[
+//             Expanded(
+//               child: Center(
+//                 child: _cameraPreviewWidget(),
+//               ),
+//             ),
+//           ],
+//         ),
+//       )
+//     );
+//   }
+
+//   Widget _cameraPreviewWidget() {
+//     return CameraPreview(
+//       controller!,
+//     );
+//   }
+// }
 
 /// Default App
 class MainApp extends StatelessWidget {
